93ff47f4ec6333f2ff32088e1be61ee8f9cd773f
Prince Kumar <kumarprince1407@gmail.com>
2023-07-17 15:35:00 +0530

    Added initial commit

---
+++ HotelReservation/src/main/java/com/bridgelabz/hotel_reservation/HotelReservation.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.hotel_reservation;
+
+public class HotelReservation {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Hotel Reservation Program!");
+    }
+}
